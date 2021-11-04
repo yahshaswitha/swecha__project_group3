8deb774c681b5eb332108e145966cf61aa0f0b86
yahshaswitha <93731585+yahshaswitha@users.noreply.github.com>
2021-11-05 00:54:23 +0530

    Create darkmode.js

---
+++ js/darkmode.js
@@ -0,0 +1,55 @@
+let task = document.getElementById("task");
+task.addEventListener("click", function (e) {
+	let addTxt = document.getElementById("addTxt");
+	let notes = localStorage.getItem("notes");
+	if (notes == null) {
+		notesObj = [];
+	} else {
+		notesObj = JSON.parse(notes);
+	}
+	notesObj.push(addTxt.value);
+	localStorage.setItem("notes", JSON.stringify(notesObj));
+	addTxt.value = "";
+	showNotes();
+});
+
+function showNotes() {
+	let notes = localStorage.getItem("notes");
+	if (notes == null) {
+		notesObj = [];
+	} else {
+		notesObj = JSON.parse(notes);
+	}
+	let html = "";
+	notesObj.forEach(function (element, index) {
+		html += `
+            <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h4 class="card-title">Task ${index + 1}</h4>
+                        <p class="card-text"> ${element}</p>
+                        <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
+                    </div>
+                </div>`;
+	});
+
+	let notesElm = document.getElementById("notes");
+	if (notesObj.length != 0) {
+		notesElm.innerHTML = html;
+	} else {
+		notesElm.innerHTML = `No Tasks`;
+	}
+}
+
+function deleteNote(index) {
+
+	let notes = localStorage.getItem("notes");
+	if (notes == null) {
+		notesObj = [];
+	} else {
+		notesObj = JSON.parse(notes);
+	}
+
+	notesObj.splice(index, 1);
+	localStorage.setItem("notes", JSON.stringify(notesObj));
+	showNotes();
+}
